3f03d36a986c6685e68424ef2801ec7e69e3980a
Felipe da Rosa Oliveira <felipe.darosaoliveira@gmail.com>
2023-08-11 17:16:11 -0300

    start form

--- src/main/java/br/com/senai/contactapp/infra/ContactController.java
+++ src/main/java/br/com/senai/contactapp/infra/ContactController.java
@@ -2,6 +2,7 @@ package br.com.senai.contactapp.infra;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class ContactController {
 	@GetMapping()
 	public String show() {
 		return "contacts/list";
+	}
+	
+	@GetMapping("/form/{id}")
+	public String form(@PathVariable String id) {
+		return "contacts/form";
 	}
 }
